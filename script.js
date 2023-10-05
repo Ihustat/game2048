dd488246b470d467de4b02b1f51df69771acd246
Ihustat <ger26dan@gmail.com>
2023-10-05 14:24:19 +0300

    add tile in random cell

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { Grid } from './modules/grid.js';
+import { Tile } from './modules/tile.js';
 
 const gameBoard = document.querySelector('.game-board');
 
 const grid = new Grid(gameBoard);
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
